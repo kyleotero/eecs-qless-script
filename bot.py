986cda3d37fd2e1e1c3fa4e299a9d7977ef02104
Mohammad Mahfooz <95328615+mahfoozm@users.noreply.github.com>
2023-01-17 09:33:13 -0500

    cleanup

--- bot.py
+++ bot.py
@@ -6,6 +6,13 @@ import os
 from bs4 import BeautifulSoup
 import time
 
+FIRST_NAME = "first nam"
+LAST_NAME = "last nam"
+PHONE_NUM = "1234567890"
+STUDENT_NUM = "123456789"
+EMAIL = "email@gmail.com"
+SERVICE_BTN_ID = "tt9000004114"
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--disable-dev-shm-usage")
@@ -36,16 +43,16 @@ except:
 firstName = driver.find_element("id", "consumerfield_firstName")
 lastName = driver.find_element("id", "consumerfield_lastName")
 phoneNum = driver.find_element("id", "consumerfield_phone")
-firstName.send_keys("first name")
-lastName.send_keys("last name")
-phoneNum.send_keys("1234567890")
+firstName.send_keys(FIRST_NAME)
+lastName.send_keys(LAST_NAME)
+phoneNum.send_keys(PHONE_NUM)
 
 nextBtn = driver.find_element("id", "qBtnNext")
 nextBtn.click()
 time.sleep(1)
 
 studentNum = driver.find_element("id", "customscreenfield_StudentID")
-studentNum.send_keys("123456789")
+studentNum.send_keys(STUDENT_NUM)
 nextBtn.click()
 time.sleep(1)
 
@@ -53,11 +60,11 @@ select = Select(driver.find_element("id","customscreenfield_Interaction_0"))
 select.select_by_visible_text('Telephone Call')
 
 email = driver.find_element("id", "customscreenfield_Email_0")
-email.send_keys("email@gmail.com")
+email.send_keys(EMAIL)
 nextBtn.click()
 time.sleep(1)
 
-serviceBtn = driver.find_element("id", "tt9000004114")
+serviceBtn = driver.find_element("id", SERVICE_BTN_ID)
 serviceBtn.click()
 time.sleep(1)
 
