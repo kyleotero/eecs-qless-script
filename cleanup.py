19e2f1cc7ea33e5cdea6d3203ceb17fdfb6d0ca5
Mohammad Mahfooz <95328615+mahfoozm@users.noreply.github.com>
2023-08-31 19:22:09 -0400

    cleanup: oops

--- cleanup.py
+++ cleanup.py
@@ -1,9 +1,20 @@
 from crontab import CronTab
+import os
+
+def schedule_cronjob(script_path, username):
+    cron = CronTab(user=username)
+    job = cron.new(command=f'/usr/bin/python3 {script_path}')
+    job.setall('5 9 * * *')  # Schedule for 9:05 AM
+    cron.write()
 
 def remove_all_cronjobs(username):
-    cron = CronTab(user='opc')
+    cron = CronTab(user=username)
     cron.remove_all()
     cron.write()
 
 if __name__ == '__main__':
-    remove_all_cronjobs('opc')  # Replace with the desired username
+    script_path = os.path.abspath(__file__)  # Get the absolute path of this script
+    username = 'ubuntu'
+    
+    remove_all_cronjobs(username)
+    schedule_cronjob(script_path, username)
