b0779047c7477ab5ab30af17e6b311d0e92c2a21
Mohammad Mahfooz <95328615+mahfoozm@users.noreply.github.com>
2023-08-31 20:07:59 -0400

    app: fix success model behavior

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    success_modal = False
     if request.method == 'POST':
         global FIRST_NAME, LAST_NAME, PHONE_NUM, STUDENT_NUM, EMAIL, SERVICE_BTN_ID
         FIRST_NAME = request.form['first_name']
@@ -14,19 +15,21 @@ def index():
         STUDENT_NUM = request.form['student_num']
         EMAIL = request.form['email']
         SERVICE_BTN_ID = request.form['service_btn_id']
-        
-        schedule_cron_job()
 
-    return render_template('index.html')
+        schedule_cron_job()
+        success_modal = True
+
+    return render_template('index.html', show_modal=success_modal)
 
 def schedule_cron_job():
-    cron_command = f'/usr/bin/python3 /home/mohammad/eecs-qless-script/bot.py {FIRST_NAME} {LAST_NAME} {PHONE_NUM} {STUDENT_NUM} {EMAIL} {SERVICE_BTN_ID}'
-    cron = CronTab(user='opc')
+    cron_command = f'/usr/bin/python3 /home/ubuntu/eecs-qless-script/bot.py {FIRST_NAME} {LAST_NAME} {PHONE_NUM} {STUDENT_NUM} {EMAIL} {SERVICE_BTN_ID}'
+    cron = CronTab(user='ubuntu')
 
     job = cron.new(command=cron_command)
-    job.setall('0 9 * * *') 
+    job.setall('0 9 * * *')
 
+    print(cron_command, flush=True)
     cron.write()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
