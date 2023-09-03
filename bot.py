d7703658683c0c909b897337d57bcae712455cb8
Mohammad Mahfooz <95328615+mahfoozm@users.noreply.github.com>
2023-09-02 22:13:51 -0400

    bot: add logging

--- bot.py
+++ bot.py
@@ -1,80 +1,100 @@
 import sys
+import logging
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import Select
 from webdriver_manager.chrome import ChromeDriverManager
-import os
 from bs4 import BeautifulSoup
 import time
 
-
-if len(sys.argv) != 7:
-    print("Usage: python3 bot.py FIRST_NAME LAST_NAME PHONE_NUM STUDENT_NUM EMAIL SERVICE_BTN_ID")
-    sys.exit(1)
-
-FIRST_NAME = sys.argv[1]
-LAST_NAME = sys.argv[2]
-PHONE_NUM = sys.argv[3]
-STUDENT_NUM = sys.argv[4]
-EMAIL = sys.argv[5]
-SERVICE_BTN_ID = sys.argv[6]
-
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument('--headless=new')
-
-link = "https://kiosk.ca1.qless.com/kiosk/app/home/19713"
-
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()), options=chrome_options
+log_filename = "bot_log.txt"
+logging.basicConfig(
+    filename=log_filename,
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s]: %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
 )
 
-driver.get(link)
+def main():
+    try:
+        if len(sys.argv) != 7:
+            logging.error("Usage: python3 bot.py FIRST_NAME LAST_NAME PHONE_NUM STUDENT_NUM EMAIL SERVICE_BTN_ID")
+            sys.exit(1)
 
-content = driver.page_source
-soup = BeautifulSoup(content, "html.parser")
+        FIRST_NAME = sys.argv[1]
+        LAST_NAME = sys.argv[2]
+        PHONE_NUM = sys.argv[3]
+        STUDENT_NUM = sys.argv[4]
+        EMAIL = sys.argv[5]
+        SERVICE_BTN_ID = sys.argv[6]
 
-time.sleep(3)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument('--headless=new')
 
-try:
-    closed_page = driver.find_element("id", "page_closed")
-    while closed_page:
-        time.sleep(30)
-        driver.refresh()
-        closed_page = driver.find_element("id", "page_closed")
-except:
-    pass
+        link = "https://kiosk.ca1.qless.com/kiosk/app/home/19713"
 
-firstName = driver.find_element("id", "consumerfield_firstName")
-lastName = driver.find_element("id", "consumerfield_lastName")
-phoneNum = driver.find_element("id", "consumerfield_phone")
-firstName.send_keys(FIRST_NAME)
-lastName.send_keys(LAST_NAME)
-phoneNum.send_keys(PHONE_NUM)
+        driver = webdriver.Chrome(
+            options=chrome_options
+        )
 
-nextBtn = driver.find_element("id", "qBtnNext")
-nextBtn.click()
-time.sleep(1)
+        driver.get(link)
 
-studentNum = driver.find_element("id", "customscreenfield_StudentID")
-studentNum.send_keys(STUDENT_NUM)
-nextBtn.click()
-time.sleep(1)
+        content = driver.page_source
+        soup = BeautifulSoup(content, "html.parser")
 
-select = Select(driver.find_element("id","customscreenfield_Interaction_0"))
-select.select_by_visible_text('Telephone Call')
+        time.sleep(5)
 
-email = driver.find_element("id", "customscreenfield_Email_0")
-email.send_keys(EMAIL)
-nextBtn.click()
-time.sleep(1)
+        try:
+            closed_page = driver.find_element("id", "page_closed")
+            while closed_page:
+                logging.info("Queue is closed")
+                time.sleep(30)
+                driver.refresh()
+                closed_page = driver.find_element("id", "page_closed")
+        except:
+            pass
 
-serviceBtn = driver.find_element("id", SERVICE_BTN_ID)
-serviceBtn.click()
-time.sleep(1)
+        firstName = driver.find_element("id", "consumerfield_firstName")
+        lastName = driver.find_element("id", "consumerfield_lastName")
+        phoneNum = driver.find_element("id", "consumerfield_phone")
+        firstName.send_keys(FIRST_NAME)
+        lastName.send_keys(LAST_NAME)
+        phoneNum.send_keys(PHONE_NUM)
 
-exitBtn = driver.find_element("id", "btnExit")
-exitBtn.click()
+        logging.info("Starting bot")
+        nextBtn = driver.find_element("id", "qBtnNext")
+        nextBtn.click()
+        time.sleep(1)
 
-driver.quit()
+        studentNum = driver.find_element("id", "customscreenfield_StudentID")
+        studentNum.send_keys(STUDENT_NUM)
+        nextBtn.click()
+        time.sleep(1)
+
+        select = Select(driver.find_element("id", "customscreenfield_Interaction_0"))
+        select.select_by_visible_text('Telephone Call')
+
+        email = driver.find_element("id", "customscreenfield_Email_0")
+        email.send_keys(EMAIL)
+        nextBtn.click()
+        time.sleep(1)
+
+        serviceBtn = driver.find_element("id", SERVICE_BTN_ID)
+        serviceBtn.click()
+        time.sleep(1)
+
+        exitBtn = driver.find_element("id", "btnExit")
+        exitBtn.click()
+
+        driver.quit()
+
+        logging.info("Script completed")
+        
+    except Exception as e:
+        logging.exception("An error occurred:")
+        raise
+
+if __name__ == "__main__":
+    main()
